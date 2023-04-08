01e2718b87493fc1ca47adacab3f32d0b9b73724
Chansoo Han <devhan1207@gmail.com>
2023-04-07 22:28:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-function sumItems(array) {
-  // Sum all the numbers in the array
+unction sumItems(array) {
+  let sum = 0;
+  for(let element of array) {
+    if(Array.isArray(element)) {
+      sum += sumItems(element);
+    } else {
+      sum += element;
+    }
+  }
+  return sum;
 }
+
+console.log(sumItems([[1, 2, [[3], 4]], 5, []]));
 
 module.exports = sumItems;
